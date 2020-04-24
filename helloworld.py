dcd31475bc36c0db48db695a5e5b5ce380506c69
arif0790 <kuet0909026@gmail.com>
2020-04-24 21:14:41 +0600

    removed redundant code from hello world.

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,1 @@
 print("hello world")
-
-DNA = "caaaaataaacataggaacggatctataaatggagacttggtcccgcgcg"
-
-Revcom = DNA.translate(str.maketrans("acgt", "tgca"))
-
-RNA = DNA.replace('t', 'u')
-
-print(DNA)
-print(Revcom)
-print(RNA)
