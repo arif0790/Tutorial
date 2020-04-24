a8743d3dbf4101f0f667e6ba7ee3411b39915b62
arif0790 <kuet0909026@gmail.com>
2020-04-24 21:10:49 +0600

    added DNA manipulation file

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,11 @@
 print("hello world")
 
+DNA = "caaaaataaacataggaacggatctataaatggagacttggtcccgcgcg"
+
+Revcom = DNA.translate(str.maketrans("acgt", "tgca"))
+
+RNA = DNA.replace('t', 'u')
+
+print(DNA)
+print(Revcom)
+print(RNA)
